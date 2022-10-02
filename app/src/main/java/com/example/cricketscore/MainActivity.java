0f397ac80ba0417dd92af6b664badef2e05369d9
letsmakecakes <adwaithrajeev@gmail.com>
2022-10-02 17:55:21 +0530

    Second Commit

--- app/src/main/java/com/example/cricketscore/MainActivity.java
+++ app/src/main/java/com/example/cricketscore/MainActivity.java
@@ -10,7 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.exit(0);
-        System.exit(0);
     }
 }
