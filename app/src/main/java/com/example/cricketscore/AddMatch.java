f81bc61a582380563c5aeb27a654d0be470578cc
letsmakecakes <adwaithrajeev@gmail.com>
2022-10-03 18:32:49 +0530

    New changes

--- app/src/main/java/com/example/cricketscore/AddMatch.java
+++ app/src/main/java/com/example/cricketscore/AddMatch.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class AddMatch extends AppCompatActivity {
+    EditText host, away, overs;
+    RadioGroup tossWon, optedFor;
     public void next(View view){
         Intent intent=new Intent(AddMatch.this,AddPlayers.class);
         startActivity(intent);
@@ -15,5 +20,15 @@ public class AddMatch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_match);
+        host = (EditText) findViewById(R.id.host_team);
+        away = (EditText) findViewById(R.id.away_team);
+        overs = (EditText) findViewById(R.id.overs);
+        tossWon = (RadioGroup) findViewById(R.id.toss_won);
+        optedFor = (RadioGroup) findViewById(R.id.opted_for);
+    }
+
+    public void reset(View view) {
+        host.setText("");
+        away.setText("");
     }
 }
--- app/src/main/java/com/example/cricketscore/TeamStats.java
+++ app/src/main/java/com/example/cricketscore/TeamStats.java
@@ -7,12 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class TeamStats extends AppCompatActivity {
-
-    public void submit(View view){
-        Intent intent=new Intent(TeamStats.this,PlayerStats.class);
-        startActivity(intent );
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
